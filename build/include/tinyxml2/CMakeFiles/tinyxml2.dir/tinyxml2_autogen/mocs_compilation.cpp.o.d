a5c3c7387e67bcb87cef12a4e31c9e4d9ca28a4d
xlzz <72382941+xlzz520@users.noreply.github.com>
2024-03-28 15:28:36 +0800

    Delete build directory

--- build/include/tinyxml2/CMakeFiles/tinyxml2.dir/tinyxml2_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-include/tinyxml2/CMakeFiles/tinyxml2.dir/tinyxml2_autogen/mocs_compilation.cpp.o: \
-  /Users/xlzz/Desktop/cg/wr/PathTracer/build/include/tinyxml2/tinyxml2_autogen/mocs_compilation.cpp
